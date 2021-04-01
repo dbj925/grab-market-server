5b048da631094ac0ce6aa26a63c8d5ea97c7ffbe
dbj925 <dbj925@naver.com>
2021-04-01 15:24:56 +0900

    데이터베이스 서버 오

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
  const express = require('express');
  const cors = require('cors');
  const app = express();
+ const models = require('./models');
  const port = 8080;
 
 
@@ -8,48 +9,76 @@
  app.use(cors());
 
  app.get("/products",(req,res) =>{
-     const query = req.query;
-     console.log("query :", query);
-     res.send({
-         "products" :[
-            {
-                "id" : 1,
-              "name": "농구공",
-              "price": 100000,
-              "seller": "조던",
-              "imageUrl": "images/products/basketball1.jpeg"
-            },
-            {
-                "id": 2,
-              "name": "축구공",
-              "price": 50000,
-              "seller": "메시",
-              "imageUrl": "images/products/soccerball1.jpg"
-            },
-            {
-                "id" : 3,
-              "name": "키보드",
-              "price": 10000,
-              "seller": "그랩",
-              "imageUrl": "images/products/keyboard1.jpg"
-            }
-      ]
-     });
+     models.Product.findAll({
+         order : [['createdAt','DESC']],
+         attributes :[
+             'id',
+             'name',
+             'price',
+             'createdAt',
+             'seller',
+             'imageUrl'
+         ]
+     })
+     .then((result) =>{
+        console.log("PRODUCTS :", result);
+        res.send({
+            products : result
+        })
+     }).catch((error) =>{
+        console.error(error);
+        res.send('에러 발생!');
+     })
  })
 
  app.post("/products",(req,res) => {
      const body = req.body;
-     res.send(
-         body,
-     );
+     const {name,description,price,seller} = body;
+     if(!name || !description || !price || !seller){
+         res.send('모든  필드를 입력해 주세요');
+     }
+     models.Product.create({
+         name,
+         description,
+         price,
+         seller
+     }).then((result) =>{
+        console.log("상품 생성 결과 :",result);
+        res.send({
+            result,
+        });
+     })
+     .catch((error) => {
+         console.error(error);
+         res.send('상품 업로드에 문제가 발생했습니다.');
+     })
  })
 
-app.get("/products/:id/events/:eventId",(req,res) => {
+app.get("/products/:id",(req,res) => {
     const params = req.params;
-    const {id,eventId} =params
-    res.send(`id는 ${id}와 ${eventId} 입니다.`);
+    const {id} =params;
+    models.Product.findOne({
+        where :{
+            id : id
+        }
+    }).then((result) => {
+        console.log("PRODUCT :", result);
+        res.send({
+            product : result,
+        })
+    }).catch((error)=>{
+        console.error(error);
+        res.send('상품 에러가 발생했습니다.');
+    })
 })
 
  app.listen(port, ()=>{
      console.log("grab의 쇼핑몰 서버가 돌아가고 있습니다.");
+     models.sequelize.sync().then(()=>{
+         console.log('DB 연결성공!')
+     }).catch((err)=>{
+         console.err(err);
+         console.log('DB 연결오류')
+         process.exit();
+     });
  })
